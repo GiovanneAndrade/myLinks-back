f1a9b08797a6f290632d643436efd06523ae828b
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-29 23:16:59 -0300

    sparkles:	feat:  repository users

--- src/repositores/user.repository.ts
+++ src/repositores/user.repository.ts
@@ -1,9 +1,20 @@
 import prisma from "../database/db";
 
-async function getUserRepository() {
-  const result = await prisma.user.findMany({});
+async function getUserRepository(email:string) {
+  const result = await prisma.user.findUnique({ where: { email: email } });
+
+  return result;
+}
+async function signupRepository( name:string, email:string, hashedPassword:string ) {
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email:email,
+      password: hashedPassword,
+    },
+  });
 
   return result;
 }
 
-export { getUserRepository };
+export { getUserRepository, signupRepository };
