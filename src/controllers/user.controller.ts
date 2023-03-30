643d9820c936088a11e4d870d00f9a0e550a1980
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-29 23:14:24 -0300

    sparkles:	feat:  users controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,48 @@
-import { Request, Response } from 'express'
-import * as allUser from '../repositores/user.repository'
+import { Request, Response } from "express";
+import * as allUser from "../services";
+import bcrypt from "bcrypt";
+import {
+  ifNotFoundError,
+  InternalServerError,
+  NotFoundError,
+} from "../erros/erros";
 
-async function getUserController(req:Request, res:Response) {
-try {
-  const result = await allUser.getUserRepository()
-    return res.send(result)
-} catch (error) {
-  return res.sendStatus(500).send(error)
-}
+async function signinController(req: Request, res: Response) {
+  const { email, password } = req.body;
+  //const userId = req.user.userId;
+
+  try {
+    const result = await allUser.signinService(email, password);
+    return res.json({ token: result });
+  } catch (error: any) {
+    if (error.statusCode === 404) return ifNotFoundError(res, error);
+
+    return InternalServerError(res);
+  }
 }
 
-export {
-  getUserController
+async function signupController(req: Request, res: Response) {
+  const {
+    name,  
+    email,
+    password,
+  }: { name: string; email: string; password: string } = req.body;
+  
+  try {
+    if (!email || !password) throw new NotFoundError("Dados inválidos.");
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await allUser.signupService(name, email, hashedPassword);
+    return res.sendStatus(201);
+  } catch (error) {
+    if (error.statusCode === 404) return ifNotFoundError(res, error);
+    if (error.code === "P2002") {
+      return res
+        .status(400)
+        .json({ error: "Error", message: "Este email já está em uso." });
+    }
+    return InternalServerError(res);
+  }
 }
+
+export { signinController, signupController };
