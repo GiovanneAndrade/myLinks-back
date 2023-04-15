3cafd9b11f6ebdccd43a864c678fda98ff4a27c4
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-15 12:07:08 -0300

    :test_tube:	test: factories user

--- tests/factories/user.factories.ts
+++ tests/factories/user.factories.ts
@@ -1,15 +1,28 @@
 import bcrypt from "bcrypt";
 import { faker } from "@faker-js/faker";
 import prisma from "../../src/database/db";
-
+import jwt from "jsonwebtoken";
+const secretKey = process.env.SECRET_KEY;
 export async function createUserFactories() {
   const password = 'gege'
   const hashedPassword = await bcrypt.hash(password, 10);
   return prisma.user.create({
     data: {
-      email: "gege@gege.com",
+      email: "gege@gege.com.br",
       name: "gege",
       password: hashedPassword,
+    },
+  });
+}
+ 
+
+export async function createSessionFactories(userId:any) {
+  const token = jwt.sign({ id: userId }, secretKey);
+ 
+  return prisma.session.create({
+    data: {
+      token:  token,
+      userId:Number(userId)
     },
   });
 }
