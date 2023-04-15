d3f8fd79218741d7d8f98c77feeea434f4662435
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-15 12:02:08 -0300

    sparkles:	feat:  repository  user types

--- src/repositores/user.repository.ts
+++ src/repositores/user.repository.ts
@@ -1,11 +1,12 @@
 import prisma from "../database/db";
+import { User } from "../protocols";
 
-async function getUserRepository(email:string) {
+async function getUserRepository(email:string){
   const result = await prisma.user.findUnique({ where: { email: email } });
-
+ 
   return result;
 }
-async function signupRepository( name:string, email:string, hashedPassword:string ) {
+async function signupRepository( name:string, email:string, hashedPassword:string ): Promise<User>{
   const result = await prisma.user.create({
     data: {
       name,
