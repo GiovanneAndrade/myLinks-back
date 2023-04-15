7af6a4fcdd4fa623711879f64ecf45ea0d6aec70
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-15 12:06:08 -0300

    sparkles:	feat:  inserting user logged in session table

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,4 +1,5 @@
 import { conflictError, NotFoundError } from "../erros/erros";
+import { Token, User } from "../protocols";
 import * as allUsers from "../repositores";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -8,16 +9,21 @@ async function signinService(email: string, password: string) {
   const user = await allUsers.getUserRepository(email);
   if (!user) throw new NotFoundError("Usurario não Cadastrado");
 
-   if (!bcrypt.compareSync(password, user.password))
-    throw new NotFoundError("Senha incorreta."); 
+  if (!bcrypt.compareSync(password, user.password))
+    throw new NotFoundError("Senha incorreta.");
 
   const token = jwt.sign({ id: user.id }, secretKey, {
     expiresIn: "4h",
   });
 
   const session = await allUsers.sessionsRepository(token, user.id);
-
-  return token;
+  let result = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    token: token,
+  };
+  return result;
 }
 async function signupService(
   name: string,
