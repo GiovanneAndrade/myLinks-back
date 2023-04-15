25a92aceb25cb5026857b1322877a514a5b10576
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-15 12:04:56 -0300

    sparkles:	fix:  services  links

--- src/services/link.services.ts
+++ src/services/link.services.ts
@@ -1,23 +1,33 @@
 import { conflictError, NotFoundError } from "../erros/erros";
-import * as allUsers from "../repositores";
+import * as allLinks from "../repositores";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import metaFetcher from "meta-fetcher";
+import { Link, Links, MetaDados, PageData } from "../protocols";
 
-
-async function linkService() {
-  const result = await allUsers.linkRepository();
-  const links = result.map((link) => link.metaFetcher);
+async function linkService(userId: string) {
+  const result = await allLinks.linkRepository(userId);
+  const links = result.map((link) => link.metaFetcher) as any;
+ 
   return links;
 }
 
-
-async function createLinkService(link: string, userId: string) {
-  link = await metaFetcher(link) as any;
-  const { metadata } = link as any;
-  const result = await allUsers.createLinkRepository(metadata, userId);
+async function createLinkService(
+  link: string,
+  userId: string
+): Promise<MetaDados> {
+  const linkMetaDados = (await metaFetcher(link)) as PageData;
+  const { metadata } = linkMetaDados as PageData;
+  const result = await allLinks.createLinkRepository(metadata, userId);
 
   return result;
 }
+async function deleteLinkService(links: Links[]): Promise<string> {
+  for (let i = 0; i < links.length; i++) {
+    await allLinks.deleteLinkRepository(Number(links[i].linkId));
+  }
 
-export { linkService, createLinkService };
+  return "ok";
+}
+
+export { linkService, createLinkService, deleteLinkService };
